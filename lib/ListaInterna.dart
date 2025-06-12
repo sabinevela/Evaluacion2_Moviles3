c29ecb5cc84fc0021d35b9d682bcec4acc9165a7
sabinevela <lukehemmingsteamomucho2005@gmail.com>
2025-06-12 09:02:39 -0500

    subido

---
+++ lib/ListaInterna.dart
@@ -0,0 +1,44 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+
+class Lista2 extends StatelessWidget {
+  const Lista2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: lista(context),
+    );
+  }
+}
+
+Future<List> leerJson(context) async {
+  String jsonString = await DefaultAssetBundle.of(context).loadString("assets/video_juegos.json");
+  Map datos = json.decode(jsonString);
+  return datos["videojuegos"];
+}
+
+Widget lista(context) {
+  return FutureBuilder(
+    future: leerJson(context),
+    builder: (context, snapshot) {
+      if (snapshot.connectionState== ConnectionState.waiting){
+        return CircularProgressIndicator();
+      }
+      if (snapshot.hasData) {
+        final data = snapshot.data!;
+        return ListView.builder(
+          itemCount: data.length,
+          itemBuilder: (context, index) {
+            final item = data[index];
+            return ListTile(
+              title: Text(item['titulo']),
+            );
+          },
+        );
+      } else {
+        return Text("No se encontró datos");
+      }
+    },
+  );
+}
